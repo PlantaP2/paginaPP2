ae2c2a77169a74d0034dcf4d6b2b4a782496001e
Hector984 <hectoruam96@gmail.com>
2022-05-23 17:21:07 -0500

    Ultimos cambios

--- resources/views/components/principal.blade.php
+++ resources/views/components/principal.blade.php
@@ -24,14 +24,32 @@
   </div><!--fin carrusel-->
 
   <main class="container-xl my-5"><!--contenido principal-->
-    <h1 class="text-center ">Bienvenido a la PP2</h1>
+    
     <div class="row mt-5">
-      <div class="col-12 d-flex flex-row justify-content-evenly">
+      <div class="col-12 d-flex flex-row justify-content-evenly mt-5">
         <div class="d-flex flex-column justify-content-center">
           <img src="./images/logo-izt.png" alt="" class="img-fluid bg-primary rounded">
           <h4 class="text-uppercase mt-3 text-center" style="color:#509f4a;">iztapalapa</h4>
         </div>
-        <h3 class="ms-2">ÁREA DE INGENIERÍA EN RECURSOS ENERGÉTICOS</h3>
+        
+        <div class="mt-0 mt-md-4 mt-lg-5">
+          <div class="">
+            <p class="h3"><strong>Bienvenido a la PP2</strong></p>
+          </div>
+          <div>
+            <p class="h4">ÁREA DE INGENIERÍA EN RECURSOS ENERGÉTICOS</p>
+          </div>
+        </div>
+        
+      </div>
+
+      <div class="col-12 my-5 text-center">
+        <p class="h4">Termodinámica, Turbomaquinaría y Energías Sustentables</p>
+        <img src="./images/logo.png" alt="" class="img-fluid">
+      </div>
+
+      <div class="col-12">
+
       </div>
     </div>
     <div class="  shadow-lg pb-5 mb-3 bg-white" >
@@ -44,10 +62,10 @@
         <div class="carousel-inner">
           <div class="carousel-item active" data-bs-interval="10000">
             <div class="row justify-content-center my-5 align-items-center" id="bienvenida"><!--inicio contenido pestaña-->
-              <h2 class="text-center fw-bold  mb-5 ">Te damos la bienvenida</h2>
+              <h2 class="text-center fw-bold  mb-5">Te damos la bienvenida</h2>
               <img src="images/principal/bienvenida.jpg" class="img-fluid col-md-5 mb-5 mb-md-0" alt="">
               <div class="col-md-6">
-                <p class="">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci, suscipit similique ab quam quasi non nostrum possimus dignissimos ad in cupiditate laborum sed repudiandae impedit voluptas cumque modi? Necessitatibus, exercitationem.</p>
+                <p class="">Generar y aplicar el conocimiento de la termodinámica constructal, del análisis de exergía avanzada y de la exergoeconomía-ambiental a sistemas termodinámicos, tales como plantas térmicas, ciclos Rankine orgánicos, motores de avión, sistemas de refrigeración, plantas de tratamiento de agua, entre otros; con la finalidad de determinar la calidad de la energía. </p>
                 <p class="">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci, suscipit similique ab quam quasi non nostrum possimus dignissimos ad in cupiditate laborum sed repudiandae impedit voluptas cumque modi? Necessitatibus, exercitationem.</p>
                 <p class="">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci, suscipit similique ab quam quasi non nostrum possimus dignissimos ad in cupiditate laborum sed repudiandae impedit voluptas cumque modi? Necessitatibus, exercitationem.</p>
               </div> 
@@ -55,31 +73,38 @@
           </div>
           <div class="carousel-item " data-bs-interval="10000">
             <div class="row justify-content-center my-5 align-items-center" id="bienvenida"><!--inicio contenido pestaña-->
-              <h2 class="text-center fw-bold  mb-5 ">Impacto social</h2>
-              <img src="images/principal/impacto.jpg" class="img-fluid col-md-5 mb-5 mb-md-0" alt="">
+              <h2 class="text-center fw-bold  mb-5 ">Objetivos</h2>
+              <img src="images/principal/objetivos.jpg" class="img-fluid col-md-5 mb-5 mb-md-0" alt="">
               <div class="col-md-6">
-                <p class="">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci, suscipit similique ab quam quasi non nostrum possimus dignissimos ad in cupiditate laborum sed repudiandae impedit voluptas cumque modi? Necessitatibus, exercitationem.</p>
-                <p class="">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci, suscipit similique ab quam quasi non nostrum possimus dignissimos ad in cupiditate laborum sed repudiandae impedit voluptas cumque modi? Necessitatibus, exercitationem.</p>
-                <p class="">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci, suscipit similique ab quam quasi non nostrum possimus dignissimos ad in cupiditate laborum sed repudiandae impedit voluptas cumque modi? Necessitatibus, exercitationem.</p>
-                <p class="">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci, suscipit similique ab quam quasi non nostrum possimus dignissimos ad in cupiditate laborum sed repudiandae impedit voluptas cumque modi? Necessitatibus, exercitationem.</p>
+                <p class="h5">Generar y aplicar el conocimiento de la termodinámica constructal, del análisis de exergía avanzada y de la exergoeconomía-ambiental a sistemas termodinámicos, tales como plantas térmicas, ciclos Rankine orgánicos, motores de avión, sistemas de refrigeración, plantas de tratamiento de agua, entre otros; con la finalidad de determinar la calidad de la energía. </p>
+                <p class="h5">Estudiar dentro del campo de la turbomaquinaria las inestabilidades de compresores centrífugos, y proponer el rediseño (revamp) de los impulsores para ampliar la región de operación estable.</p>
+                <p class="h5">Producir y caracterizar biocombustibles de segunda y tercera generación y evaluar el desempeño de motores de combustión interna como una alternativa sustentable de estos sistemas.</p>
               </div> 
             </div><!--fin contenido pestaña-->
           </div>
             <div class="carousel-item  data-bs-interval="10000"">
               <div class="row justify-content-center my-5 align-items-center" id="bienvenida"><!--inicio contenido pestaña-->
-                <h2 class="text-center fw-bold  mb-5 ">Contactos</h2>
+                <h2 class="text-center fw-bold  mb-5 ">Formación de recursos humanos</h2>
                 <div class=" col-md-5 row mb-5 mb-md-0 ">
-                  <img src="https://scontent.fmex34-1.fna.fbcdn.net/v/t1.18169-9/390302_138786489558101_1730006680_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeHyZXFiwbkBZ4GhwsAJjJ8W-9u4HQ_G-0j727gdD8b7SCBflOFjCa-pVd2mj8cWnzk8vWn0qkI8tIc41uAojc9R&_nc_ohc=AGk-JLTDwNAAX9xC18z&_nc_ht=scontent.fmex34-1.fna&oh=00_AT-32M5ZRx5YC3Z8sHkZd0IQRt-yKgniVZO3qv9DBptLKg&oe=62ACE003"  class="img-fluid col-4 " alt="">
-                  <img src="https://scontent.fmex34-1.fna.fbcdn.net/v/t39.30808-6/244627907_4648448548521757_8111407843219712119_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeF2sID7G9dOsgA1LVJ-DqcQxRv70aIL6Y7FG_vRogvpjl0veJ8F3Z5_rIDV195v4zW_amDy1MMwEs-wYIypDWHf&_nc_ohc=IuTTG9PYIZkAX_BgyzK&_nc_ht=scontent.fmex34-1.fna&oh=00_AT8xJW2EFoEYCw5XqXVAyw12GNJPBg9d_qQgL9M0wWjE4A&oe=628B4A6A"  class="img-fluid col-4" alt="">
-                  <img src="https://scontent.fmex34-1.fna.fbcdn.net/v/t1.6435-9/39846706_122524915357572_8901528971766661120_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeG0XFGQg4rja9DK61oOutu38Y73_bb_Qkvxjvf9tv9CS8S20cCJSO14T3Ax5PUo99pPaUAEX9J1WwNtEoeeCWvh&_nc_ohc=APTNdkfBdbcAX9lhrIq&_nc_ht=scontent.fmex34-1.fna&oh=00_AT_F0-qItWQU2IZjnG1iSNro-sYO3FOd9Chd5igA4JYcFw&oe=62AE131D"  class="img-fluid col-4 " alt="">
+                  <img src="https://plazalindavista.mx/wp-content/uploads/2019/03/CFE.jpg"  class="img-fluid col-4 " alt="">
+                  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/General_Electric_logo.svg/1200px-General_Electric_logo.svg.png"  class="img-fluid col-4" alt="">
+                  <img src="https://globalenergy.mx/wp-content/uploads/2019/01/transcanada_logo_2c_pos_1585x400.jpg"  class="img-fluid col-4 " alt="">
                   <img src="https://scontent.fmex34-1.fna.fbcdn.net/v/t1.6435-9/118769554_3181272161959011_6327723297110086161_n.png?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGiIfU3bMv-6k8Gk9NB5x7NXqBeca4B4xxeoF5xrgHjHMYrbc094TixJiTS-4NkmJ25_sCnsTSWPpTIyjhXyf9L&_nc_ohc=2nsOA1dVu54AX_rhXvZ&_nc_ht=scontent.fmex34-1.fna&oh=00_AT_Cn3GBfQAbymI7ohO9wsArdY6Y4MdYpnzHDO5hIX_NrQ&oe=62AC1D4A"  class="img-fluid col-4 " alt="">
                   <img src="https://scontent.fmex34-1.fna.fbcdn.net/v/t1.18169-9/13239275_1312769305417035_8335649673795505072_n.png?_nc_cat=1&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeEBIGoDAD6-dJUaVquWPlLG44vxknf2gPTji_GSd_aA9J2RA4_rvMHqZAMADKzqWu0jUt29t_RGedJdd-MdzOhQ&_nc_ohc=N_rR7KrJCO4AX-SGgoa&_nc_ht=scontent.fmex34-1.fna&oh=00_AT9YgsVAO26TcU4F3G72vGIqF32oLlyosp9x6MKnPHGThg&oe=62AB97D0"  class="img-fluid col-4" alt="">
                   <img src="https://scontent.fmex34-1.fna.fbcdn.net/v/t1.6435-9/159027257_10157814696456615_4514470716038115023_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGP2aGn0CZrMXxt-leGcGpQxgKxieooRE7GArGJ6ihETj3HYfbHSUQhv2Vhsq3LHVEUKXXedxoMZxnrCzoxdRYg&_nc_ohc=PMHIWSHGadIAX_R_kpb&_nc_ht=scontent.fmex34-1.fna&oh=00_AT8RjkyqsnBkhiAHU17Qi5KVi09SOz5Ksn2Vy-ZlnoKlcg&oe=62AC737E"  class="img-fluid col-4 " alt="">
 
                 </div>
                 <div class="col-md-6">
-                  <p class="">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci, suscipit similique ab quam quasi non nostrum possimus dignissimos ad in cupiditate laborum sed repudiandae impedit voluptas cumque modi? Necessitatibus, exercitationem.</p>
-                  <p class="">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci, suscipit similique ab quam quasi non nostrum possimus dignissimos ad in cupiditate laborum sed repudiandae impedit voluptas cumque modi? Necessitatibus, exercitationem.</p>
+                  <ul class="h5" style="list-style-type: circle;">
+                    <li>1 doctorado</li>
+                    <li>18 maestría</li>
+                    <li>122 licenciatura</li>
+                    <li>1 Libro de docencia</li>
+                    <li>350 congresos nacionales e internacionales</li>
+                    <li>48 Artículos publicados de investigación en diferentes revistas</li>
+                    <li>22 Artículos publicados de investigación (JCR) 7 en el periodo de 2020-2022</li>
+                  </ul>
+                  <p class="h5">La mayoría de los alumnos que han terminado en la PP2 trabajan en empresas transnacionales y nacionales: GE, Trans Canadá, PEMEX, CFE, Mitsui &CO,  Iberdrola, etc.</p>
                 </div> 
               </div><!--fin contenido pestaña-->
             </div>
